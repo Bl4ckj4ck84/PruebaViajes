6e0994022f04ebaa6f1800241e5c1b7561e77ba1
Bl4ckj4ck84 <cristian.chavez.jara@gmx.com>
2024-04-05 12:07:45 -0300

    modificaciones varias

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
+// Crear un objeto de ScrollSpy para el body, con el target especificado
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: "#navbar-example",
+    target: "#navbar-example",  // El elemento al que se hará scroll
   });
   
-  
+  // Obtener una lista de todos los elementos con data-bs-toggle="tooltip"
   var tooltipTriggerList = [].slice.call(
     document.querySelectorAll('[data-bs-toggle="tooltip"]')
   );
+  
+  // Crear una instancia de Tooltip para cada elemento de la lista
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
-
-  $('#Enviar').on('click', function () {
-    alert('El correo fue enviado correctamente...');
+  
+  // Obtener el botón con el id "enviarCorreo"
+  let enviarCorreoBtn = document.getElementById("enviarCorreo");
+  
+  // Agregar un evento "click" al botón "enviarCorreo"
+  $('#enviarCorreo').on('click', function () {
+    // Mostrar una alerta cuando se haga clic en el botón
+    alert('El mensaje fue enviado correctamente...');
   });
+  
